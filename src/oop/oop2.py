6c3db45a76fd9cca6862ef7610234079885d7b25
zakaria touzri <zakadmaster@gmail.com>
2020-07-10 17:32:19 +0200

    ADDED: default num_wheels

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,11 +4,14 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
-    # TODO
 
+zak = GroundVehicle(11)
+
+print(zak.num_wheels)
 
 # Subclass Motorcycle from GroundVehicle.
 #
@@ -19,13 +22,13 @@ class GroundVehicle():
 
 # TODO
 
-vehicles = [
-    GroundVehicle(),
-    GroundVehicle(),
-    Motorcycle(),
-    GroundVehicle(),
-    Motorcycle(),
-]
+# vehicles = [
+#     GroundVehicle(),
+#     GroundVehicle(),
+#     Motorcycle(),
+#     GroundVehicle(),
+#     Motorcycle(),
+# ]
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
